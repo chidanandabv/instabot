2ad24706efb0ae523adf1d4923b6d102548e598f
chidanandabv <chidanandabv23@gmail.com>
2023-10-22 14:40:58 -0300

    make folloing bot

---
+++ bit.py
@@ -0,0 +1,10 @@
+from instabot import bot
+my_bot=bot()
+
+#login cantain
+
+my_bot.login(username="iam.chida1",password="chidananda@2004")
+
+#follw the users
+my_bot.follow_users(["ibe.cn","_its_me_dev.12","abhilash23_abhi"])
+my_bot.unfollow_non_followers()
