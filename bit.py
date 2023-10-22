4730e95b9e3569670b801068f8abddff8b7feb86
chidanandabv <chidanandabv23@gmail.com>
2023-10-22 14:56:07 -0300

    make dm  bot

--- bit.py
+++ bit.py
@@ -7,9 +7,16 @@ my_bot.login(username="iam.chida1",password="chidananda@2004")
 
 #follw the users
 my_bot.follow_users(["ibe.cn","_its_me_dev.12","abhilash23_abhi"])
-my_bot.unfollow_non_followers()
+#my_bot.unfollow_non_followers()
 
 #unfollow
 
 my_bot.unfollow_non_nonfollowers()
 
+#uploading img
+
+my_bot.upload_photo("/home/chidananda/Downloads/photo.png",caption="life is fair when you love your self")
+
+#sending a message to user
+
+my_bot.send_message("hey bro i am a bot ","ibe.cn") 
