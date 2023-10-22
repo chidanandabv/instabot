9bc8c2f991c6c0bdc63172caa803ad8bbe98a2b6
chidanandabv <chidanandabv23@gmail.com>
2023-10-22 14:51:33 -0300

    make uploading photo  bot

--- bit.py
+++ bit.py
@@ -8,3 +8,8 @@ my_bot.login(username="iam.chida1",password="chidananda@2004")
 #follw the users
 my_bot.follow_users(["ibe.cn","_its_me_dev.12","abhilash23_abhi"])
 my_bot.unfollow_non_followers()
+
+#unfollow
+
+my_bot.unfollow_non_nonfollowers()
+
